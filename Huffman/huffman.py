e9ace86cb7742ffca3765d528782a1f3bea5b544
Kamil Jurek <kamiljurek624@gmail.com>
2017-10-17 00:21:49 +0200

    compression added

--- Huffman/huffman.py
+++ Huffman/huffman.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from operator import *
+import itertools
 
 filename = "seneca.txt"
-N = 2
+N = 5
 
 class Node(object):
 	left = None
@@ -28,11 +29,19 @@ frequency = defaultdict(int)
 for symbol in splitteddata:
     frequency[symbol] += 1
 
+#a = map(''.join, itertools.product(''.join(frequency1.keys()), repeat=N))
+#print(list(a))
+
+frequency = defaultdict(int)
+for symbol in splitteddata:
+    frequency[symbol] += 1
+
 sorted_x = sorted(frequency.items(), key=lambda x:x[1])
 nodesList = []
 for key, value in sorted_x:
-    n = Node(key, value)
-    nodesList.append(n)
+    if(value > 0):
+        n = Node(key, value)
+        nodesList.append(n)
 
 while(len(nodesList) > 1):
     nodesList.sort(key=lambda x: x.weight, reverse=False)
@@ -54,37 +63,48 @@ def codeIt(s, node):
 		codeIt(s+"1", node.right)
 
 codeIt("",nodesList[0])
-print(codes)
-reverseCodes= {y:x for x,y in codes.items()}
-codedData = "".join([codes[a] for a in data])
-print(reverseCodes)
 
+
+import json
+json.dump(codes, open("codes.txt",'w'))
+codes = json.load(open("codes.txt"))
+
+codedData = "".join([codes[x] for x in splitteddata])
+
+# first split into 8-bit chunks
+bit_strings = [codedData[i:i + 8] for i in range(0, len(codedData), 8)]
+byte_list = [int(b, 2) for b in bit_strings]
+ba = bytearray(byte_list)
+#print([b for b in ba])
+with open('compressed.bin', 'wb') as f:
+	f.write(ba)  	# convert to bytearray before writing
+
+#############################
+print("wspolczynnik kompresji: " + str((len(data)+len(codedData))/len(data)))
+#############################
+# print(c)
+# print(codedData)
+data = open("compressed.bin", "rb").read()
+ba = bytearray(data)
+bins = ['{:08b}'.format(x) for x in ba]
+# intsss = [int.from_bytes(x, byteorder='big') for x in ba]
+#print(bit_strings)
+#print([b for b in bins])
+dataC = "".join(bins)
+# print(len(dataC))
+# print(len(codedData))
+#
+reverseCodes= {y:x for x,y in codes.items()}
+#print(reverseCodes)
 current_code = ""
 decoded_text = ""
-for bit in codedData:
+for bit in dataC:
     current_code += bit
     if(current_code in reverseCodes):
     	character = reverseCodes[current_code]
     	decoded_text += character
     	current_code = ""
-print("Decoded:" + decoded_text)
+#print("Decoded:" + decoded_text)
 file = open('decompresed.txt', 'w')
 file.write(decoded_text)
-file.close()
-
-
-print(data)
-print(codedData)
-print("wspolczynnik: " + str((len(data)+len(codedData))/len(data)))
-
-# for key, value in sorted_x:
-#     print(key, value)
-
-for k,v in codes.items():
-     print("'" + k + "'\t" + v)
-#for symbol, weight in so.items():
-#    print("'" + str(symbol) + "': " + str(weight))
-
-file = open('compresed.txt', 'w')
-file.write(codedData)
 file.close()
